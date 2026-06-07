ff3bbafac2ff81d2a1dbdd7e1622d3e5f144ef6a
Ryvix <ryvixenterprise4@gmail.com>
2026-06-06 17:14:52 -0700

    Update app.run to use dynamic port configuration
    Retrieve port from environment variable for deployment.

--- run.py
+++ run.py
@@ -3,4 +3,6 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    # On récupère le port de Render, sinon 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
