568a361a201a9fad8db032d2a929f40c267d11e8
Ryvix <ryvixenterprise4@gmail.com>
2026-06-06 18:45:12 -0700

    Update run.py

--- run.py
+++ run.py
@@ -3,10 +3,28 @@ import os
 
 app = create_app()
 # AJOUTEZ TEMPORAIRE
+# 2. Automatisation pour le Plan Free de Render
 with app.app_context():
-    # Crée les tables si elles n'existent pas
+    # Crée les tables dans la base PostgreSQL de Render si elles n'existent pas
     db.create_all()
-    print(" Base de données vérifiée/initialisée.")
+    print("Base de données initialisée (Tables créées).")
+
+    # Création automatique du compte admin pour le premier test
+    if not User.query.filter_by(username="admin").first():
+        admin = User(
+            username="admin",
+            email="admin@ryvixlab.com",
+            role="administrateur",
+            actif=True
+        )
+        # Assurez-vous que votre modèle User a bien la méthode set_password
+        admin.set_password("@admin12345")
+        
+        db.session.add(admin)
+        db.session.commit()
+        print(" Compte administrateur créé par défaut.")
+    else:
+        print(" Le compte admin existe déjà, aucune action requise.")
 # FIN
 
 if __name__ == '__main__':
